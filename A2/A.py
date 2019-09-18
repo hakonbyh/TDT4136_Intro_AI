95c9deb0d81f76bbfb0b327412826a2d967ff0b4
Jørgen Weidemann <jorgenweidemann@Jrgens-MacBook-Pro-2.local>
2019-09-18 19:13:27 +0200

    Start of technical environment.

---
+++ A2/A.py
@@ -0,0 +1,15 @@
+"""
+A* is the algoritm required to use in assignment 2
+"""
+
+
+# This class will contian the A* algorithm.
+class A():
+
+    # Constructor.
+    def __init__(self):
+        
+        # Initializes closed and open lists
+        open, closed = [],
+
+        
---
+++ A2/Main.py
@@ -0,0 +1,12 @@
+"""
+Main of assignment 2.
+"""
+
+import A
+import Map
+
+
+
+if __name__ == "__main__":
+    map = Map_Obj()
+    map.show.map
